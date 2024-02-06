3aae33d975a8cbc1490fe1971bfe0176fb4a6607
MMaraP <dmara@unsa.edu.pe>
2024-02-05 22:21:16 -0500

    diseño de login-usuario y serparacion del script

---
+++ js/login-usuario.js
@@ -0,0 +1,23 @@
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener('submit', evento => {
+    evento.preventDefault();
+    const formData = new FormData(loginForm);
+    
+    var url = './cgi-bin/login.pl';
+    var promise = fetch(url, {
+        method: "post",
+        body: formData,
+    });
+    promise.then(response => response.text())
+    .then(data => {
+        if(data === "OK"){
+            window.location.href = "./clientes.html";
+        } else {
+            var element = document.getElementById('error-box');
+            element.innerHTML = "<p>(!) Usuario no encontrado</p>";
+        }
+        console.log(data);
+    }).catch(error => {
+        console.log('Error:', error);
+    });
+});
