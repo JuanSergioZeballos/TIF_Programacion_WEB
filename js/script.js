ed73220f3833038e87aaf4e5f3674a43c6fdb84d
MMaraP <dmara@unsa.edu.pe>
2024-02-05 20:02:11 -0500

    modificacion el javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-document.getElementById('readMore').addEventListener('click', function () {
-    document.getElementById('fullDescription').style.display = 'block';
+document.getElementById('read-more').addEventListener('click', function () {
+    var fullDescription = document.getElementById('full-description');
+    var readMore = document.getElementById('read-more');
+
+    fullDescription.style.display = 'block';
+
+    readMore.style.display = 'none';
+});
+
+document.getElementById('disguise').addEventListener('click', function () {
+    var fullDes = document.getElementById('full-description');
+    var readM = document.getElementById('read-more');
+    fullDes.style.display = 'none';
+    readM.style.display = 'block'
 });
